4fb54f9aea5d6d8fe185e85f5ce8178ba6d97f40
WilliamTrung <williamthanhtrungq2@gmail.com>
2022-09-26 20:08:02 +0700

    Add UserService

---
+++ Service/Services/Service/UserService.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Context;
+using ApplicationCore.Models;
+using AutoMapper;
+using Service.Service;
+using Service.Services.IService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.Service
+{
+    public class UserService : BaseService<DTO.User, User>, IUserService
+    {
+        private FoodyContext _context;
+        private IMapper _mapper;
+        public UserService(IMapper mapper, FoodyContext context) : base(mapper, context)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+    }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -2,13 +2,25 @@
 using ApplicationCore.Context;
 using ApplicationCore.Models;
 using ApplicationCore.Repository;
+using AutoMapper;
+using Service;
+using Service.Services.IService;
+using Service.Services.Service;
 
 string connection = "Server=tcp:foodyfpt.database.windows.net,1433;Initial Catalog=dbfoodyfpt;Persist Security Info=False;User ID=trungntse151134fpt;Password=Loliizabezt.1211;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 FoodyContext _context = new FoodyContext(connection);
 IGenericRepository<Product> _repo = new GenericRepository<Product>(_context);
+/*
+Mapping mapping = new Mapping();
+IMapper mapper = new Mapper(typeof(Mapping));
+
+IProductService productService = new ProductService(mapping, _context);
+
+
 
 var list = _repo.GetList();
 foreach(var item in list.Result.ToList())
 {
     Console.WriteLine(item);
 }
+*/
