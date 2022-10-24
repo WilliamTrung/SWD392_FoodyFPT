b0078386ec8bcc2820f28f0eaba9c6eaf26c7223
WilliamTrung <williamthanhtrungq2@gmail.com>
2022-10-24 13:44:33 +0700

    Add blob for products

---
+++ FoodyAPI/Helper/Azure/Blob/Blob.cs
@@ -0,0 +1,76 @@
+﻿using Azure.Storage.Blobs;
+using FoodyAPI.Helper.Azure.IBlob;
+using Service.Services.IService;
+using Service.Services.Service;
+using System.ComponentModel;
+
+namespace FoodyAPI.Helper.Azure.Blob
+{
+    public class Blob : IBlobService
+    {
+        IConfiguration _config;
+
+        private BlobServiceClient? blobStorage;
+        private BlobContainerClient? blobContainer;
+
+        public Blob(IConfiguration config)
+        {
+            _config = config;
+            blobStorage = AzureService.GetBlobServiceClient(_config);
+            var blobStorage_config = _config.GetSection("BlobStorage");
+        }
+        public List<string>? GetURLs(string container, int id)
+        {
+            List<string>? result = null;
+            if(blobStorage != null)
+                blobContainer = AzureService.CheckBlobContainerAsync(blobStorage, container).Result;
+            if (blobContainer == null)
+                return result;
+            
+            string prefix = id + "_";
+            var blobs = blobContainer.GetBlobs(prefix: prefix);
+            string url_prefix = blobContainer.Uri.ToString();
+            result = new List<string>();
+            foreach (var blob in blobs)
+            {
+                string url = url_prefix + "/" + blob.Name;
+                result.Add(url);
+            }
+            //result = blob.Uri.ToString();
+            return result;
+        }
+
+        public async Task<bool> UploadAsync(List<IFormFile> files, string container, int id)
+        {
+            try
+            {
+                if (blobStorage == null)
+                    return false;
+                blobContainer = AzureService.CheckBlobContainerAsync(blobStorage, container).Result;
+                if (blobContainer != null)
+                {
+                    int i = 0;
+                    string prefix = id.ToString() + "_";
+                    foreach (var file in files)
+                    {
+                        string extension = file.ContentType.Split("/")[1];
+                        string filename = id + "_" + (++i) + "." + extension;
+                        var blob = blobContainer.GetBlobClient(filename);
+                        var stream = file.OpenReadStream();
+                        await blob.UploadAsync(stream, overwrite: true);
+
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FoodyAPI/Helper/Azure/IBlob/IBlobService.cs
@@ -0,0 +1,8 @@
+﻿namespace FoodyAPI.Helper.Azure.IBlob
+{
+    public interface IBlobService
+    {
+        public Task<bool> UploadAsync(List<IFormFile> files, string container, int id);
+        public List<string>? GetURLs(string container, int id);
+    }
+}
